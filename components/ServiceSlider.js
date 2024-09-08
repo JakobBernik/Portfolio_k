f7a0c6ceb4e5423088f2fefa8c158e7d9dd700a0
Jakob Bernik <jakobb.bernik@gmail.com>
2024-09-08 11:14:42 +0200

    added work page

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -68,7 +68,7 @@ const ServiceSlider = () => {
       {
         serviceData.map((item,index)=>{
           return <SwiperSlide key={index}>
-            <div className="bg-[rgba(190,118,140,0.3)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(33,49,62,0.3)] transition-all duration-300">
+            <div className="bg-[rgba(190,118,140,0.5)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(33,49,62,0.5)] transition-all duration-300">
               {/* icon */}
               <div className="text-4xl text-[#f53a52] mb-4">{item.icon}</div>
               {/* tittle and desc */}
