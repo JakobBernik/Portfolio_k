e56c4c6048e91dc76dacc34f760a568a018fadf8
Jakob Bernik <jakobb.bernik@gmail.com>
2024-09-10 12:32:48 +0200

    added image imports

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,8 @@ import Link from "next/link";
 //components
 import Socials from '../components/Socials';
 
+import logo from '../public/logo_j.png';
+
 const Header = () => {
   return (
     <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
@@ -14,7 +16,7 @@ const Header = () => {
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           {/* logo */}
           <Link href={'/'}>
-            <Image unoptimized src={'/logo_j.png'} width={150} height={25} alt="" priority={true}/>
+            <Image unoptimized src={logo} width={150} height={25} alt="" priority={true}/>
           </Link>
           {/* socials */}
           <Socials />
